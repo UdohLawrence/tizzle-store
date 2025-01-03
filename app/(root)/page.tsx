71adc874313e66b46a08d04b9b21b3afff0a2b44
Udoh Lawrence <lawrenceaudoh@gmail.com>
2025-01-03 14:05:56 +0100

    create a root for the whole site

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button"
-
 const Homepage = () => {
   return (
     <>
       <h1 className="text-red-500 text-5xl">Tizzle Stores</h1>
-      <Button>Click Me</Button>
     </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Tizzle Stores",
-  description:
-    "Shop a diverse range of products including cosmetics, food, electronic gadgets, fashion apparel, home goods, books, toys, sports equipment, pet supplies, automotive accessories, and much more. Discover quality items and great deals in one place.",
+  title: {
+    template: `%s | Tizzle`,
+    default: APP_NAME
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
