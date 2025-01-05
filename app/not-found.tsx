00c09b644dc18b64e856738081d7cefb72230910
Udoh Lawrence <lawrenceaudoh@gmail.com>
2025-01-05 12:21:59 +0100

    FEATURE: add code for the notfound page

---
+++ app/not-found.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen">
+      <Image
+        priority={true}
+        src='/images/logo.svg'
+        width={60}
+        height={60}
+        alt={`${APP_NAME} logo`}
+      />
+      <div className="p-6 rounded-lg w-1/3 shadow-md text-center">
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">Could not find the requested page</p>
+        <Button
+          variant='outline'
+          className="mt-4 ml-3"
+          onClick={() => window.location.href = '/'}
+        >
+          Back to Home
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
