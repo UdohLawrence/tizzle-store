a5e816f4f9049da6a6eb7c125d590380615445e8
Udoh Lawrence <lawrenceaudoh@gmail.com>
2025-01-07 07:47:52 +0100

    implement limits to the displayed data on the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
 
   return (
-    <>
-      <h1 className="text-red-500 text-5xl">Tizzle Stores</h1>
-    </>
+    <div className="space-y-8">
+      <h1 className="h2-bold">Latest Product</h1>
+      <ProductList title="Newest Arrivals" data={sampleData.products} limit={4} />
+    </div>
   );
 };
 
