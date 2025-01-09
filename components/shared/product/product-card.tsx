9df645e3d4326767fcda5d112c84f807ee6002f2
Udoh Lawrence <lawrenceaudoh@gmail.com>
2025-01-09 11:05:34 +0100

    render the product price as a component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import ProductPrice from './product-price';
 
 const ProductCard = ({product}: {product: any}) => {
   return (
@@ -32,9 +33,7 @@ const ProductCard = ({product}: {product: any}) => {
             {product.rating} stars
           </p>
           {product.stock > 0 ? (
-            <p className="font-bold">
-              {product.price}
-          </p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
               <p className="text-destructive">
                 Out of Stock
