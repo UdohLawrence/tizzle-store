cd606e293893336eef0a5f9b55049738c3389854
Udoh Lawrence <lawrenceaudoh@gmail.com>
2025-01-10 08:47:21 +0100

    add a constant for fetching latest products

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,3 +3,5 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Tizzle Store'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Shop a diverse range of products including cosmetics, food, electronic gadgets, fashion apparel, home goods, books, toys, sports equipment, pet supplies, automotive accessories, and much more. Discover quality items and great deals in one place.'
 
 export const SERVER_URL = process.env.NEXT_SERVER_URL || 'http://localhost:3000'
+
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
