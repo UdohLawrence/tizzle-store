1ce6016e07f7a1acd7fafb49fbec058989e74862
Udoh Lawrence <lawrenceaudoh@gmail.com>
2025-01-21 16:17:17 +0100

    FIX: link to the homepage on the not-found component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@
 
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
@@ -17,13 +17,12 @@ const NotFound = () => {
       <div className="p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Not Found</h1>
         <p className="text-destructive">Could not find the requested page</p>
-        <Button
-          variant='outline'
+        <Link
           className="mt-4 ml-3"
-          onClick={() => window.location.href = '/'}
+          href = '/'
         >
           Back to Home
-        </Button>
+        </Link>
       </div>
     </div>
   )
